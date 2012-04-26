6eeb714949362cbec0f9d5adc03a8773213d3f23
Myron Marston <myron.marston@gmail.com>
2012-04-26 09:27:06 -0700

    Detect and prevent circular dependencies.

--- lib/plines/dependency_graph.rb
+++ lib/plines/dependency_graph.rb
@@ -1,9 +1,14 @@
+require 'set'
+
 module Plines
   # Represents a dependency graph of Plines steps. This graph contains
   # StepInstances (i.e. Step classes paired with data). The graph
   # takes care of preventing duplicate step instances.
   class DependencyGraph
     attr_reader :steps
+
+    # Raised when a circular dependency is detected.
+    class CircularDependencyError < StandardError; end
 
     def initialize(batch_data)
       @steps = StepInstance.accumulate_instances do
@@ -15,6 +20,31 @@ module Plines
             end
           end
         end
+      end
+
+      detect_circular_dependencies!
+    end
+
+  private
+
+    def detect_circular_dependencies!
+      @visited_steps = Set.new
+
+      @steps.each do |step|
+        next if @visited_steps.include?(step)
+        depth_first_search_from(step)
+      end
+    end
+
+    def depth_first_search_from(step, current_stack=Set.new)
+      @visited_steps << step
+
+      if current_stack.include?(step)
+        raise CircularDependencyError, "Your graph appears to have a circular dependency: #{current_stack.inspect}"
+      end
+
+      step.dependencies.each do |dep|
+        depth_first_search_from(dep, current_stack | [step])
       end
     end
   end
--- spec/unit/plines/dependency_graph_spec.rb
+++ spec/unit/plines/dependency_graph_spec.rb
@@ -4,25 +4,7 @@ require 'plines'
 module Plines
   describe DependencyGraph do
     describe ".new" do
-      step_class(:A) { depends_on :B, :C, :D }
-      step_class(:B) { depends_on :E }
-
-      step_class(:C) do
-        depends_on :E, :F
-        fan_out do |data|
-          [data + 1, data + 2]
-        end
-      end
-
-      step_class(:D); step_class(:E);
-
-      step_class(:F) do
-        fan_out do |data|
-          [data * 1, data * 2]
-        end
-      end
-
-      let!(:graph) { DependencyGraph.new(10) }
+      let(:graph) { DependencyGraph.new(10) }
 
       let(:steps_by) do
         Hash.new do |h, (klass, data)|
@@ -35,6 +17,24 @@ module Plines
       end
 
       it 'constructs a full dependency graph from the given declarations' do
+        step_class(:A) { depends_on :B, :C, :D }
+        step_class(:B) { depends_on :E }
+
+        step_class(:C) do
+          depends_on :E, :F
+          fan_out do |data|
+            [data + 1, data + 2]
+          end
+        end
+
+        step_class(:D); step_class(:E);
+
+        step_class(:F) do
+          fan_out do |data|
+            [data * 1, data * 2]
+          end
+        end
+
         graph.steps.should eq([
           step(A),
           step(B), step(C, 11), step(C, 12), step(D),
@@ -62,6 +62,22 @@ module Plines
         step(F, 10).dependees.to_a.should =~ [step(C, 11), step(C, 12)]
         step(F, 20).dependencies.to_a.should =~ []
         step(F, 20).dependees.to_a.should =~ [step(C, 11), step(C, 12)]
+      end
+
+      it 'detects direct circular dependencies' do
+        step_class(:X) { depends_on :Y }
+        step_class(:Y) { depends_on :X }
+
+        expect { graph }.to raise_error(DependencyGraph::CircularDependencyError)
+      end
+
+      it 'detects indirect circular dependencies' do
+        step_class(:W) { depends_on :X }
+        step_class(:X) { depends_on :Y }
+        step_class(:Y) { depends_on :Z }
+        step_class(:Z) { depends_on :W }
+
+        expect { graph }.to raise_error(DependencyGraph::CircularDependencyError)
       end
     end
   end
