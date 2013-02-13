29236a1ab7bfa7a5dcd8e3495bb15be311309505
Patrick Roby <patrick@seomoz.org>
2013-02-13 15:55:23 -0800

    add test to verify args are getting passed to the underlying redis objects

--- spec/unit/plines/redis_objects_spec.rb
+++ spec/unit/plines/redis_objects_spec.rb
@@ -8,18 +8,31 @@ module Plines
   describe RedisObjectsHelpers, :redis do
     class DefaultKeyPrefix < Struct.new(:pipeline, :id)
       include Plines::RedisObjectsHelpers
-      counter :a_counter
+      value :a_value
+      lock :a_lock, expiration: 900, timeout: 60
+      list :a_list, marshal: true
       attr_reader :redis
 
       def initialize(pipeline, id)
         super(pipeline, id)
         @redis = pipeline.redis
-        a_counter.increment # have to increment to create the key
+        a_value.set(99) # have to set a value to creat the key
       end
     end
 
     class OverrideKeyPrefix < DefaultKeyPrefix
       redis_key_prefix 'override'
+    end
+
+    it 'passes additional args to the base redis::object' do
+      obj = DefaultKeyPrefix.new(pipeline_module, '1234')
+
+      lock_options = obj.a_lock.options
+      expect(lock_options[:expiration]).to be(900)
+      expect(lock_options[:timeout]).to be(60)
+
+      list_opions = obj.a_list.options
+      expect(list_opions[:marshal]).to be_true
     end
 
     it 'contains the pipeline name, class name, and id in the key prefix' do
